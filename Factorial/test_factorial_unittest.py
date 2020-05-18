52e1ddaed3920bf18c0f1ad7c83440a85e14bacb
timandrews <tandrews75@hotmail.com>
2020-05-18 16:40:02 -0400

    added separate tests with unittest and pytest for factorial.py

--- Factorial/test_factorial_unittest.py
+++ Factorial/test_factorial_unittest.py
@@ -1,27 +1,26 @@
 import unittest
 import math
-from factorial import my_factorial
 import factorial
+
 
 
 class TestFactorial(unittest.TestCase):
     def test_zero(self):
-        result = my_factorial(0)
+        result = factorial.my_factorial(0)
         self.assertEqual(result, math.factorial(0))
 
     def test_positive(self):
-        result = my_factorial(10)
+        result = factorial.my_factorial(10)
         self.assertEqual(result, math.factorial(10))
 
     def test_ValueError(self):
         with self.assertRaises(ValueError):
-            my_factorial(-10)
+            factorial.my_factorial(-10)
 
     def test_TypeError(self):
         with self.assertRaises(TypeError):
             factorial.my_factorial('a string')
-            my_factorial(1.5)
-
+            factorial.my_factorial(1.5)
 
 if __name__ == '__main__':
     unittest.main()
